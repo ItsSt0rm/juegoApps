94645423da5ed8e6fb1e79d42fb08b9d2d143e53
ItsSt0rm <esteban.palaciov@upb.edu.co>
2020-03-09 23:24:43 -0500

    Avance con la programación

--- js/main.js
+++ js/main.js
@@ -1,6 +1,27 @@
 
     var secciones = [];
     var tiempo_splash = 2000;
+    var imagenes = [];
+    var nivel = 2; //Se obtiene del localstorage
+
+    //Imágenes
+    imagenes[1] = "img/jugar/img/unicornio1.png";
+    imagenes[2] = "img/jugar/img/unicornio2.png";
+    imagenes[3] = "img/jugar/img/unicornio3.png";
+    imagenes[4] = "img/jugar/img/unicornio4.png";
+    imagenes[5] = "img/jugar/img/dragon1.png";
+    imagenes[6] = "img/jugar/img/dragon2.png";
+    imagenes[7] = "img/jugar/img/dragon3.png";
+    imagenes[8] = "img/jugar/img/dragon4.png";
+    imagenes[9] = "img/jugar/img/sirena1.png";
+    imagenes[10] = "img/jugar/img/sirena2.png";
+    imagenes[11] = "img/jugar/img/sirena3.png";
+    imagenes[12] = "img/jugar/img/sirena4.png";
+
+
+
+
+
 
     window.onload = function () {
         inicializarReferencias();
@@ -32,4 +53,19 @@
         secciones[id_seccion].classList.add("animated");
         secciones[id_seccion].classList.add("headShake");
         secciones[id_seccion].classList.remove("oculto");
+    }
+
+    function cargarNivel(){
+        document.getElementById("niveljuego").innerHTML = nivel;
+        cargarImagenes();
+    }
+
+    function cargarImagenes(){
+         
+        var idPista = 1;
+        var indiceImg = nivel*4;
+        for (var i = indiceImg; i >= indiceImg - 3; i--){
+            document.getElementById("pista"+idPista).src=imagenes[i];
+            idPista++;
+        }
     }
