e7a5b91c4cd9b06ef094897303af115ff8edb38a
ItsSt0rm <esteban.palaciov@upb.edu.co>
2020-03-19 11:47:16 -0500

    Funcionalidad y diseño completo

--- js/main.js
+++ js/main.js
@@ -11,6 +11,9 @@ var palabraNivel;
 var letrasAleatorias;
 var contenedorAnimal;
 var letrasContenedor;
+var nivelJuego;
+var nivelJuego1;
+var monedasJuego;
 
 
 
@@ -30,27 +33,79 @@ imagenes[9] = "img/jugar/img/sirena1.png";
 imagenes[10] = "img/jugar/img/sirena2.png";
 imagenes[11] = "img/jugar/img/sirena3.png";
 imagenes[12] = "img/jugar/img/sirena4.png";
+imagenes[13] = "img/jugar/img/cerbero1.png";
+imagenes[14] = "img/jugar/img/cerbero2.png";
+imagenes[15] = "img/jugar/img/cerbero3.png";
+imagenes[16] = "img/jugar/img/cerbero4.png";
+imagenes[17] = "img/jugar/img/fenix1.png";
+imagenes[18] = "img/jugar/img/fenix2.png";
+imagenes[19] = "img/jugar/img/fenix3.png";
+imagenes[20] = "img/jugar/img/fenix4.png";
+imagenes[21] = "img/jugar/img/grifo1.png";
+imagenes[22] = "img/jugar/img/grifo2.png";
+imagenes[23] = "img/jugar/img/grifo3.png";
+imagenes[24] = "img/jugar/img/grifo4.png";
+imagenes[25] = "img/jugar/img/hidra1.png";
+imagenes[26] = "img/jugar/img/hidra2.png";
+imagenes[27] = "img/jugar/img/hidra3.png";
+imagenes[28] = "img/jugar/img/hidra4.png";
+imagenes[29] = "img/jugar/img/horus1.png";
+imagenes[30] = "img/jugar/img/horus2.png";
+imagenes[31] = "img/jugar/img/horus3.png";
+imagenes[32] = "img/jugar/img/horus4.png";
+imagenes[33] = "img/jugar/img/manticora1.png";
+imagenes[34] = "img/jugar/img/manticora2.png";
+imagenes[35] = "img/jugar/img/manticora3.png";
+imagenes[36] = "img/jugar/img/manticora4.png";
+imagenes[37] = "img/jugar/img/medusa1.png";
+imagenes[38] = "img/jugar/img/medusa2.png";
+imagenes[39] = "img/jugar/img/medusa3.png";
+imagenes[40] = "img/jugar/img/medusa4.png";
+imagenes[41] = "img/jugar/img/minotauro1.png";
+imagenes[42] = "img/jugar/img/minotauro2.png";
+imagenes[43] = "img/jugar/img/minotauro3.png";
+imagenes[44] = "img/jugar/img/minotauro4.png";
+imagenes[45] = "img/jugar/img/naga1.png";
+imagenes[46] = "img/jugar/img/naga2.png";
+imagenes[47] = "img/jugar/img/naga3.png";
+imagenes[48] = "img/jugar/img/naga4.png";
+imagenes[49] = "img/jugar/img/pegaso1.png";
+imagenes[50] = "img/jugar/img/pegaso2.png";
+imagenes[51] = "img/jugar/img/pegaso3.png";
+imagenes[52] = "img/jugar/img/pegaso4.png";
+imagenes[53] = "img/jugar/img/triton1.png";
+imagenes[54] = "img/jugar/img/triton2.png";
+imagenes[55] = "img/jugar/img/triton3.png";
+imagenes[56] = "img/jugar/img/triton4.png";
 
 //Palabra
 
 palabras[1] = "unicornio";
 palabras[2] = "dragon";
 palabras[3] = "sirena";
-
+palabras[4] = "cerbero";
+palabras[5] = "fenix";
+palabras[6] = "grifo";
+palabras[7] = "hidra";
+palabras[8] = "horus";
+palabras[9] = "manticora";
+palabras[10] = "medusa";
+palabras[11] = "minotauro";
+palabras[12] = "naga";
+palabras[13] = "pegaso";
+palabras[14] = "triton";
 
 
 window.onload = function () {
     inicializarReferencias();
     setTimeout(cambiarSplash, tiempo_splash);
     contenedorAnimal = document.getElementById("nombreanimal");
-    //nivel = localStorage.setItem("nivel", 1);
-    //monedas = localStorage.setItem("monedas", 0);
-    localStorage.clear();
+    //localStorage.clear();
     nivel = localStorage.getItem("nivel");
     if (nivel == undefined) nivel = 1;
     monedas = localStorage.getItem("monedas");
     if (monedas == undefined) monedas = 0;
-    letrasEliminadas = localStorage.getItem("letraseliminados");
+    letrasEliminadas = localStorage.getItem("letraseliminadas");
     if (letrasEliminadas == undefined) letrasEliminadas = "";
 }
 
@@ -64,6 +119,10 @@ function inicializarReferencias() {
     secciones[7] = document.getElementById("seccion_7");
 
     letrasContenedor = document.getElementById("letrascontenedor");
+    nivelJuego = document.getElementById("niveljuego");
+    nivelJuego1 = document.getElementById("niveljuego1");
+    monedasJuego = document.getElementById("monedasjuego");
+
 }
 
 function cambiarSplash() {
@@ -71,8 +130,6 @@ function cambiarSplash() {
     secciones[2].className = "home";
 }
 
-//https://developer.mozilla.org/es/docs/Web/API/Element/classList
-//https://www.youtube.com/watch?v=2z0wMNHPbzk
 function cambiarSeccion(id_seccion) {
 
     for (var i in secciones) {
@@ -89,17 +146,25 @@ function cargarNivel() {
     if (nivel == undefined) nivel = 1;
     monedas = localStorage.getItem("monedas");
     if (monedas == undefined) monedas = 0;
+    letrasEliminadas = localStorage.getItem("letraseliminadas");
+    if (letrasEliminadas == undefined) letrasEliminadas = "";
 
+    if(nivel == 15){
+        alert("¡Felicidades, has completado todos los niveles! Gracias por jugar");
+        localStorage.clear();
+        location.reload();
+    }
     palabraNivel = palabras[nivel];
     letrasOcultas = [];
-    document.getElementById("niveljuego").innerHTML = nivel;
-    document.getElementById("niveljuego1").innerHTML = nivel;
-    document.getElementById("monedasjuego").innerHTML = monedas;
+    nivelJuego.innerHTML = nivel;
+    nivelJuego1.innerHTML = nivel;
+    monedasJuego.innerHTML = monedas;
     contenedorAnimal.innerHTML = null;
     puedeAñadirLetra = true;
     contenedorAnimal.className = "fondoanimalnormal";
     cargarImagenes();
     repartirLetras();
+    cargarEliminadas();
 }
 
 function cargarImagenes() {
@@ -189,6 +254,7 @@ function verificarPalabra() {
 
         if (palabra.toLowerCase() == palabras[nivel]) {
             letrasEliminadas = "";
+            localStorage.setItem("letraseliminadas", letrasEliminadas);
             contenedorAnimal.className = "fondoanimalcorrecto";
             nuevoNivel = parseInt(nivel) + 1;
             localStorage.setItem("nivel", nuevoNivel);
@@ -197,7 +263,7 @@ function verificarPalabra() {
             setTimeout(cambiarPalabra, 800);
         } else {
             contenedorAnimal.className = "fondoanimalincorrecto";
-            console.log("Incorrecto");
+
         }
     }
 }
@@ -210,18 +276,40 @@ function cambiarPalabra() {
 
 function ayudaJuego() {
     //var splitOcultas = letrasOcultas.split("");
+    if (monedas < 5) {
+        alert("No tienes monedas suficientes");
+        return;
+    }
+
+
+
+
     for (var i = 0; i < 14; i++) {
         var letra = document.getElementById("letra" + i);
         var clasesLetra = letra.className;
         caracterLetra = letra.innerHTML.substring(0, 1);
-        console.log(letrasAleatorias);
-        console.log(caracterLetra);
         if (letrasAleatorias.indexOf(caracterLetra) != -1) {
             if ((letrasEliminadas.indexOf(caracterLetra) == -1)) {
                 letrasEliminadas += caracterLetra;
+                localStorage.setItem("letraseliminadas", letrasEliminadas);
                 letra.className = "letraeliminada " + clasesLetra;
+                monedas -= parseInt(5);
+                localStorage.setItem("monedas", monedas);
+                monedasJuego.innerHTML = monedas;
                 break;
             }
+        }
+    }
+}
+
+function cargarEliminadas() {
+    for (var i = 0; i < 14; i++) {
+        var letra = document.getElementById("letra" + i);
+        var clasesLetra = letra.className;
+        caracterLetra = letra.innerHTML.substring(0, 1);
+
+        if (letrasEliminadas.indexOf(caracterLetra) != -1) {
+            letra.className = "letraeliminada " + clasesLetra;
         }
     }
 }
